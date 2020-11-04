20f4d2971b1bdfe434857bf7f2d1dd5cedf0ee1a
jaytarang92 <jaytarang92@gmail.com>
2020-11-03 21:22:18 -0600

    mass text feature return bad request

--- SendMessageOnTime/__init__.py
+++ SendMessageOnTime/__init__.py
@@ -1,5 +1,5 @@
 from azure import functions as func
-from azure.communication.sms import PhoneNumber, SendSmsOptions, SmsClient
+from azure.communication.sms import PhoneNumber, SendSmsOptions, SmsClient, SendSmsResponse
 from datetime import datetime, timezone
 from logging import info
 from os import environ
@@ -7,18 +7,18 @@ from os import environ
 
 class SimpleMessagingService:
 
-    def __init__(self, leased_phone_number, to_phone_number, comm_service_connection_string):
+    def __init__(self, leased_phone_number, target_phone_numbers, comm_service_connection_string):
         self.leased_phone_number = leased_phone_number
-        self.to_phone_number = to_phone_number
+        self.target_phone_numbers = target_phone_numbers
         self.connection_string = comm_service_connection_string
         self.sms_client = SmsClient.from_connection_string(self.connection_string)
 
-    def send_sms(self, message_to_send: str):
+    def send_sms(self, message_to_send: str, delivery_report_bool: bool = False) -> SendSmsResponse:
         sms_response = self.sms_client.send(
             from_phone_number=PhoneNumber(self.leased_phone_number),
-            to_phone_numbers=[PhoneNumber(self.to_phone_number)],
+            to_phone_numbers=[PhoneNumber(phone_number) for phone_number in self.target_phone_numbers.split(',')],
             message=message_to_send,
-            send_sms_options=SendSmsOptions(enable_delivery_report=True))
+            send_sms_options=SendSmsOptions(enable_delivery_report=delivery_report_bool))
         return sms_response
 
 def main(sendMessageTimer: func.TimerRequest) -> None:
@@ -29,8 +29,8 @@ def main(sendMessageTimer: func.TimerRequest) -> None:
 
     sms = SimpleMessagingService(
         leased_phone_number=environ['LEASED_PHONE_NUMBER'],
-        to_phone_number=environ['TO_PHONE_NUMBER'],
+        target_phone_numbers=environ['TARGET_PHONE_NUMBERS'],
         comm_service_connection_string=environ['COMM_SERVICE_CONNECTION_STRING']
     )
-    sms.send_sms(message_to_send=f'Message from function App {utc_timestamp}')
-    info('Python timer trigger function ran at %s', utc_timestamp)
+    sms.send_sms(message_to_send='Text me back if you get this! Running locally. - Jasmit Tarang')
+    info(f'Message was sent at: {utc_timestamp}')
